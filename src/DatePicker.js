fadb663b71463b7c1371342b20a3de39e4c07bdb
Anders Hagward <taperlawyer@gmail.com>
2017-08-12 16:39:30 +0200

    Add calendar and time icons

--- src/DatePicker.js
+++ src/DatePicker.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Icon from 'react-geomicons';
 import './DatePicker.css';
 
 class DatePicker extends Component {
   render() {
     return (
-      <input className="date-picker" type="date" value={this.props.value} onChange={this.props.onChange} />
+      <div className="date-picker">
+        <div className="date-picker__icon">
+          <Icon name="calendar" />
+        </div>
+        <input className="date-picker__input" type="date" value={this.props.value} onChange={this.props.onChange} />
+      </div>
     );
   }
 }
--- src/TimePicker.js
+++ src/TimePicker.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Icon from 'react-geomicons';
 import './TimePicker.css';
 
 class TimePicker extends Component {
   render() {
     return (
-      <input className="time-picker" type="time" value={this.props.value} onChange={this.props.onChange} />
+      <div className="time-picker">
+        <div className="time-picker__icon">
+          <Icon name="clock" />
+        </div>
+        <input className="time-picker__input" type="time" value={this.props.value} onChange={this.props.onChange} />
+      </div>
     );
   }
 }
