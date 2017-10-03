1197d1b56897d431e0230e97447b6bd91bd15de9
Anders Hagward <taperlawyer@gmail.com>
2017-10-03 22:16:22 +0200

    Update trip looks

--- src/Trip.js
+++ src/Trip.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import TripDetails from './TripDetails';
-import Util from './Util';
+import TripOverview from './TripOverview';
 import './Trip.css';
 
 export default class Trip extends Component {
@@ -17,19 +17,7 @@ export default class Trip extends Component {
   render() {
     return (
       <div className="trip" onClick={this.handleClick}>
-        <div className="trip__times">
-          <div className="trip__time-origin">{this.originTime()}</div>
-          <div className="trip__time-arrow">→</div>
-          <div className="trip__time-destination">{this.destinationTime()}</div>
-          <div className="trip__time-total">Restid: {this.totalTravelTime()}</div>
-        </div>
-        <div className="trip__legs">
-          {this.props.trip.Leg.map((leg, legIndex) =>
-            <div className="trip__leg" key={legIndex} style={{backgroundColor: leg.fgColor, color: leg.bgColor}}>
-              {leg.sname ? leg.sname : 'Gå'}
-            </div>
-          )}
-        </div>
+        <TripOverview trip={this.props.trip} />
         {this.state.expanded &&
           <TripDetails trip={this.props.trip} />
         }
@@ -41,17 +29,5 @@ export default class Trip extends Component {
     this.setState(prevState => ({
       expanded: !prevState.expanded
     }));
-  }
-
-  originTime() {
-    return Util.first(this.props.trip.Leg).Origin.time;
-  }
-
-  destinationTime() {
-    return Util.last(this.props.trip.Leg).Destination.time;
-  }
-
-  totalTravelTime() {
-    return Util.timeDiff(this.originTime(), this.destinationTime());
   }
 }
---
+++ src/TripOverview.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Util from './Util';
+import './TripOverview.css';
+
+export default class TripOverview extends Component {
+  render() {
+    return (
+      <div className="trip-overview">
+        <div className="trip-overview__legs">
+          {this.props.trip.Leg
+            .filter(leg => leg.sname)
+            .map((leg, legIndex) =>
+              <div className="legs__leg" key={legIndex}
+                style={{backgroundColor: leg.fgColor, color: leg.bgColor, borderColor: leg.fgColor === '#ffffff' ? '#EE1844' : 'transparent'}}>
+                {leg.sname}
+              </div>
+            )}
+        </div>
+        <div className="trip-overview__times">
+          <div className="times__start-end">
+            <div className="times__start">{this.originTime()}</div>
+            <div className="times__arrow">→</div>
+            <div className="times__end">{this.destinationTime()}</div>
+          </div>
+          <div className="times__travel-time">{this.totalTravelTime()}</div>
+        </div>
+      </div>
+    );
+  }
+
+  originTime() {
+    return Util.first(this.props.trip.Leg).Origin.time;
+  }
+
+  destinationTime() {
+    return Util.last(this.props.trip.Leg).Destination.time;
+  }
+
+  totalTravelTime() {
+    return Util.timeDiff(this.originTime(), this.destinationTime());
+  }
+}
