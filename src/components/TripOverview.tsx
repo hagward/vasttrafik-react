9767989b1c967f977b599e781dfbc0701534f6d4
Anders Hagward <taperlawyer@gmail.com>
2018-04-01 14:21:56 +0200

    Update trip overview appearance

--- src/components/TripOverview.tsx
+++ src/components/TripOverview.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Trip } from '../Api';
+import * as FontAwesome from 'react-fontawesome';
+import { Trip, Leg, Location } from '../Api';
 import Util from '../Util';
 import './TripOverview.css';
 
@@ -9,68 +10,60 @@ interface Props {
 
 export default class TripOverview extends React.Component<Props, any> {
   render() {
+    const origin = Util.first(this.props.trip.Leg).Origin;
+    const destination = Util.last(this.props.trip.Leg).Destination;
+
     return (
       <div className="trip-overview">
-        <div className="trip-overview__legs">
-          {this.props.trip.Leg
-            .filter(leg => leg.sname)
-            .map((leg, legIndex) =>
-              <div
-                className="legs__leg"
-                key={legIndex}
-                style={{
-                  backgroundColor: leg.fgColor,
-                  color: leg.bgColor,
-                  borderColor: leg.fgColor === '#ffffff' ? '#EE1844' : 'transparent'
-                }}
-              >
-                {leg.sname}
-              </div>
-            )}
+        <div className="trip-overview__origin">
+          <div className="trip-overview__times">{this.renderTime(origin)}</div>
+          <div className="trip-overview__legs">{this.renderLegs()}</div>
         </div>
-        <div className="trip-overview__times">
-          <div className="times__start-end">
-            <div className="times__start">
-              {this.originTime()}
-              {this.delay()}
-            </div>
-            <div className="times__arrow">→</div>
-            <div className="times__end">{this.destinationTime()}</div>
-          </div>
-          <div className="times__travel-time">{this.totalTravelTime()}</div>
+        <div className="trip-overview__arrow">
+          <FontAwesome name="arrow-right" />
+        </div>
+        <div className="trip-overview__destination">
+          <div className="trip-overview__times">{this.renderTime(destination)}</div>
+          <div className="trip-overview__travel-time">Restid: {this.travelTime(origin, destination)}</div>
         </div>
       </div>
     );
   }
 
-  originTime() {
-    return Util.first(this.props.trip.Leg).Origin.time;
+  renderTime(location: Location) {
+    return [
+      <span className="trip-overview__time" key={0}>{location.rtTime}</span>,
+        location.time !== location.rtTime &&
+          <s className="trip-overview__time trip-overview__time--invalid" key={1}>{location.time}</s>,
+    ];
   }
 
-  destinationTime() {
-    return Util.last(this.props.trip.Leg).Destination.time;
+  renderLegs() {
+    return this.props.trip.Leg
+      .filter(leg => leg.sname)
+      .map((leg, index) => this.renderLeg(leg, index));
   }
 
-  totalTravelTime() {
-    const origin = Util.first(this.props.trip.Leg).Origin;
-    const destination = Util.last(this.props.trip.Leg).Destination;
-    const originDateTime = Util.toDateTime(origin.date, origin.time);
-    const destinationDateTime = Util.toDateTime(destination.date, destination.time);
-    const duration = destinationDateTime.diff(originDateTime).shiftTo('hours', 'minutes');
+  renderLeg(leg: Leg, index: number) {
+    return (
+      <span
+        className="trip-overview__leg"
+        key={index}
+        style={{
+          backgroundColor: leg.fgColor,
+          color: leg.bgColor,
+          borderColor: leg.fgColor === '#ffffff' ? '#EE1844' : 'transparent'
+        }}
+      >
+        {leg.sname}
+      </span>
+    );
+  }
+
+  travelTime(start: Location, end: Location) {
+    const startDateTime = Util.toDateTime(start.date, start.time);
+    const endDateTime = Util.toDateTime(end.date, end.time);
+    const duration = endDateTime.diff(startDateTime).shiftTo('hours', 'minutes');
     return Util.padNumber(duration.hours) + ':' + Util.padNumber(duration.minutes);
-  }
-
-  delay() {
-    const origin = Util.first(this.props.trip.Leg).Origin;
-    const dateTime = Util.toDateTime(origin.date, origin.time);
-    const realDateTime = Util.toDateTime(origin.rtDate, origin.rtTime);
-
-    if (realDateTime < dateTime) {
-      return ' (-' + dateTime.diff(realDateTime).shiftTo('minutes').minutes + ')';
-    } else if (realDateTime > dateTime) {
-      return ' (+' + realDateTime.diff(dateTime).shiftTo('minutes').minutes + ')';
-    } else {
-      return '';
-    }
   }
 }
