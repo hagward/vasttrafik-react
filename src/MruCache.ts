c01ff9deef7b484834ac0964a4449fd749d808ce
Anders Hagward <taperlawyer@gmail.com>
2018-10-20 12:11:41 +0200

    Store mru items in a variable

--- src/MruCache.ts
+++ src/MruCache.ts
@@ -3,39 +3,41 @@ interface Item {
 }
 
 export default class MruCache<T extends Item> {
+  private items: T[];
   private limit: number;
 
   constructor(limit: number) {
     this.limit = limit;
+
+    const mru = localStorage.getItem('mru');
+    this.items = mru ? JSON.parse(mru) : [];
   }
 
   add(item: T): void {
-    let mru = this.getMostRecentlyUsed();
-    this.removeExistingItem(item, mru);
-    mru.unshift(item);
-    this.ensureLengthWithinLimit(mru);
-    this.setMostRecentlyUsed(mru);
+    this.removeExistingItem(item);
+    this.items.unshift(item);
+    this.ensureLengthWithinLimit();
+    this.setMostRecentlyUsed();
   }
 
   getMostRecentlyUsed(): T[] {
-    const mru = localStorage.getItem('mru');
-    return mru ? JSON.parse(mru) : [];
+    return this.items;
   }
 
-  setMostRecentlyUsed(mru: T[]): void {
-    localStorage.setItem('mru', JSON.stringify(mru));
+  setMostRecentlyUsed(): void {
+    localStorage.setItem('mru', JSON.stringify(this.items));
   }
 
-  private removeExistingItem(item: T, mru: T[]): void {
-    const existingIndex = mru.findIndex(existingItem => existingItem.id === item.id);
+  private removeExistingItem(item: T): void {
+    const existingIndex = this.items.findIndex(existingItem => existingItem.id === item.id);
     if (existingIndex > -1) {
-      mru.splice(existingIndex, 1);
+      this.items.splice(existingIndex, 1);
     }
   }
 
-  private ensureLengthWithinLimit(mru: T[]): void {
-    if (mru.length > this.limit) {
-      mru.splice(this.limit, mru.length - this.limit);
+  private ensureLengthWithinLimit(): void {
+    if (this.items.length > this.limit) {
+      this.items.splice(this.limit, this.items.length - this.limit);
     }
   }
 }
