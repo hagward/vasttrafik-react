63742076e226aabd12cda264ac92708a1ac32f8c
Anders Hagward <taperlawyer@gmail.com>
2019-08-31 13:06:45 +0200

    Rename serviceWorker.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
-import * as serviceWorker from './registerServiceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./components/App";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('app-root'));
+ReactDOM.render(<App />, document.getElementById("app-root"));
 
 serviceWorker.register();
