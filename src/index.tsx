d182d258b1ee564be6bfb71546421e323b4831f5
Anders Hagward <taperlawyer@gmail.com>
2019-08-31 13:04:43 +0200

    Update registerServiceWorker.ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('app-root'));
-registerServiceWorker();
+
+serviceWorker.register();
