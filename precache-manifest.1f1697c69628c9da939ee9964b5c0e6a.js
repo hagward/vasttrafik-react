9166e95fc4b16952120d5e97d81dd3a36d16e70a
hagward <hagward@users.noreply.github.com>
2019-12-21 17:17:03 +0000

    Automated deployment: Sat Dec 21 17:17:03 UTC 2019 7fc41c70ff165ed2f307873d6a260493cbf284d5

---
+++ precache-manifest.1f1697c69628c9da939ee9964b5c0e6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dcf1624aa80766d04d9ce15e9fc338d",
+    "url": "/vasttrafik-react/index.html"
+  },
+  {
+    "revision": "4c38e51d159255f134e3",
+    "url": "/vasttrafik-react/static/css/main.cdacafa1.chunk.css"
+  },
+  {
+    "revision": "9957c2f469a7b2e9700f",
+    "url": "/vasttrafik-react/static/js/2.b811fd32.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/vasttrafik-react/static/js/2.b811fd32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c38e51d159255f134e3",
+    "url": "/vasttrafik-react/static/js/main.cb35964d.chunk.js"
+  },
+  {
+    "revision": "71713d470a7c318a708d",
+    "url": "/vasttrafik-react/static/js/runtime-main.3cc248d7.js"
+  }
+]);
