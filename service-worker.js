66ec78016918582cc1bd6be9787eeb9b56b9224c
hagward <hagward@users.noreply.github.com>
2019-12-22 15:54:27 +0000

    Automated deployment: Sun Dec 22 15:54:27 UTC 2019 32e442d31b4c102f510ddfb6cc6d7d5f851855ed

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vasttrafik-react/precache-manifest.a5bfe687c57b89557ce37de3ffd79c2b.js"
+  "/vasttrafik-react/precache-manifest.eb26b2775b74f8ff16d28fbd356fa135.js"
 );
 
 self.addEventListener('message', (event) => {
