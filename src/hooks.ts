3fa6b5b3c73e5eba9b6fef9a1948fc057e3af4c5
Anders Hagward <taperlawyer@gmail.com>
2020-04-19 11:42:17 +0200

    Add generic type to hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,16 +1,12 @@
 import { useEffect, useState } from "react";
 
 // https://dev.to/gabe_ragland/debouncing-with-react-hooks-jci
-export default function useDebounce(value: any, delay: number) {
-  const [debouncedValue, setDebouncedValue] = useState(value);
+export default function useDebounce<T>(value: T, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
-    return () => {
-      clearTimeout(handler);
-    };
+    const handler = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(handler);
   }, [value, delay]);
 
   return debouncedValue;
