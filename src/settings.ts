cb4a9bc07dac3105239a089d5e236bc9897163d0
Anders Hagward <taperlawyer@gmail.com>
2020-12-23 23:49:26 +0100

    Update all dependencies

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,6 @@
-export default {
-  key: 'd9k7BQPB7_5nIsjU9d7Y0IkFIqUa',
-  secret: 'Ksgx8WdEnn9gjp9Sqln72WgbQdUa',
+const settings = {
+  key: "d9k7BQPB7_5nIsjU9d7Y0IkFIqUa",
+  secret: "Ksgx8WdEnn9gjp9Sqln72WgbQdUa",
 };
+
+export default settings;
